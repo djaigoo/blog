b105902a206aae173580b13b58b6f0e3f454ac05
dai <daihao1@xdf.cn>
2023-03-31 19:12:41 +0800

    update

---
+++ _posts/gen-mysql.sh
@@ -0,0 +1,31 @@
+cmd=$1
+if [ "$cmd" == "" ];then
+  cmd=merge
+fi
+base_file=middleware/mysql/mysql讲义.md
+if [ "$cmd" == "init" ];then
+  title=$(cat ${base_file} |grep -E '^## |^# ' |sed -e 's/#/00/g' -e 's/ /-/g')
+  for t in ${title};do 
+    line=$(echo $title|sed -e 's/ /\n/g' | sed -n -e "/${t}/=")
+    new=middleware-mysql-${line}-${t}
+    old=$(ls |grep $t)
+    if [ "${old}" == "" ];then
+      echo $new
+      hexo new $new
+    else
+      echo mv $old $new.md
+      mv $old $new.md
+    fi
+  done
+fi
+
+if [ "$cmd" == "merge" ];then
+  git add middleware-mysql*
+  git commit -m 'update'
+  file=middleware-mysql.md
+  echo --- > ${file}
+  for f in $(ls |grep middleware-mysql- | sort -n -t '-' -k 3);do
+    echo $f | awk -F '-00' '{print $2}' | sed -e 's/^/#/' -e 's/00/#/g' -e 's/#-/# /' -e 's/.md$//' >> ${file}
+    sed -n -e 's/^#/###/g' -e '9,$p' $f >> ${file} 
+  done
+fi
